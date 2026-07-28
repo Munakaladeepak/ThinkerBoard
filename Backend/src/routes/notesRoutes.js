e919b47ed4502365572ea0afe6d42863280bb78f
Munakala Deepak <munakaladeepak@gmail.com>
2026-07-28 20:53:46 +0530

    fix: static path casing and import path whitespace

--- Backend/src/routes/notesRoutes.js
+++ Backend/src/routes/notesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {CreateNode, Deletenode, getAllNotes, UpdateNode,getNoteById} from "../controller/notesController.js "
+import {CreateNode, Deletenode, getAllNotes, UpdateNode,getNoteById} from "../controller/notesController.js"
 const router = express.Router();
 
 router.get("/",getAllNotes)
